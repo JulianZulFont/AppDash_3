38a41483b027d4b4010fa41f971545580de1242a
Nicolás Cárdenas <ne.cardenas@uniandes.edu.co>
2026-03-05 21:38:11 -0500

    Update theming

--- theme.py
+++ theme.py
@@ -1,9 +1,19 @@
 COLORS = {
     "primary": "rgb(157, 203, 252)",
+    "on-primary": "rgb(0, 51, 85)",
+    "secondary": "rgb(186, 200, 218)",
+    "tertiary": "rgb(212, 190, 230)",
+    "error": "rgb(255, 180, 171)",
     "background": "rgb(16, 20, 24)",
     "on-background": "rgb(224, 226, 232)",
+    "surface": "rgb(16, 20, 24)",
+    "on-surface": "rgb(224, 226, 232)",
+    "surface-variant": "rgb(66, 71, 78)",
     "on-surface-variant": "rgb(194, 199, 207)",
+    "outline": "rgb(140, 145, 153)",
     "outline-variant": "rgb(66, 71, 78)",
+
+    # Custom components
     "card": "rgba(255, 255, 255, 0.05)",
     "grid": "rgba(255, 255, 255, 0.06)",
 }
